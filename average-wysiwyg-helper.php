0dab8765337ba1a5158fde7d06081adb4326255b
joerhoney <joerhoney@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-09 20:47:27 +0000

    Changed "upgrading" to "updating" in "Thank you for updating" message.
    git-svn-id: https://plugins.svn.wordpress.org/average-wysiwyg-helper/trunk@985704 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- average-wysiwyg-helper.php
+++ average-wysiwyg-helper.php
@@ -76,7 +76,7 @@ if(!class_exists('avrgwysiwyg_class')) :
         $avrgFavsTarg = ' target="_blank"';
       }
       if ( get_site_option( $avrgwysiwyg_v ) == self::VERSION ) return;
-      $msg = sprintf(__('Thank you for upgrading Average WYSIWYG Helper! If you like this plugin, please consider <a href="%s" target="_blank">rating it</a> and trying out <a href="%s"'.$avrgFavsTarg.'>our other plugins</a>!'),'http://wordpress.org/support/view/plugin-reviews/average-wysiwyg-helper',$avrgFavs);
+      $msg = sprintf(__('Thank you for updating Average WYSIWYG Helper! If you like this plugin, please consider <a href="%s" target="_blank">rating it</a> and trying out <a href="%s"'.$avrgFavsTarg.'>our other plugins</a>!'),'http://wordpress.org/support/view/plugin-reviews/average-wysiwyg-helper',$avrgFavs);
       echo "<div class='update-nag'>$msg</div>";
       update_site_option( $avrgwysiwyg_v, self::VERSION );
     }
